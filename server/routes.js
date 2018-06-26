4f0e14b546a20f4d73a17bc81834f0f9b693393b
xinconan <xinconan2@sina.com>
2018-06-26 20:04:33 +0800

    chore: update script

--- server/routes.js
+++ server/routes.js
@@ -5,9 +5,14 @@
 const router = require('koa-router')({
   prefix: '/yh'
 })
+const notice = require('./controllers/notice')
 const lottery = require('./controllers/lottery')
 
 // 摇号楼盘
 router.get('/project', lottery.project)
+// 摇号信息
+router.get('/list', notice.list)
+// 摇号信息——子列表
+router.post('/subList', notice.subList)
 
 module.exports = router
