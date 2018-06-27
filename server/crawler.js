7a25a9ae52037fdaed16d4226df9704bde7df717
xinconan <xinconan2@sina.com>
2018-06-27 10:49:35 +0800

    feat: deprecated api

--- server/crawler.js
+++ server/crawler.js
@@ -1,14 +1,14 @@
 const request = require('request')
 const {saveLotteryInfo, saveContentInfo} = require('./dbService')
-
+// deprecated 此处官方接口已经没有了，只能以爬虫解析的方式获取，暂停本项目
 let infoUrl = 'http://www.hz-notary.com/showYhjgPage?yhjg_page.pageNum='
 const yhUrl = 'http://www.hz-notary.com/showYhjg'
 const detailUrl = 'http://www.hz-notary.com/showYhjgOne'
 let totalPage = 10
 let currentPage = 1
-async function getAll() {
+async function getAll () {
   // 先获取第一页数据，根据返回的页面数量进行抓取
-  let first = await getData({url: infoUrl+1})
+  let first = await getData({url: infoUrl + 1})
   totalPage = first.pageCnt
   savePageList(first.dataList)
   currentPage++
@@ -21,7 +21,7 @@ async function getAll() {
 }
 
 // 保存每页数据并获取内容列表保存
-async function savePageList(list) {
+async function savePageList (list) {
   const pagePromise = list.map(async item => {
     try{
       // 存储每一项
@@ -33,7 +33,7 @@ async function savePageList(list) {
         form: {'jgsc.id': item.id}
       })
       contentList = contentList.dataList
-      for(let i=0;i<contentList.length;i++) {
+      for (let i = 0; i < contentList.length; i++) {
         let info = await getData({
           url: detailUrl,
           method: 'POST',
@@ -44,7 +44,7 @@ async function savePageList(list) {
         await saveContentInfo(info)
         // console.log(info.id + ' done!')
       }
-    } catch(e) {
+    } catch (e) {
       console.log(e)
       return e
     }
@@ -56,7 +56,7 @@ async function savePageList(list) {
 }
 
 // 获取每页列表数据
-function getData(option) {
+function getData (option) {
   return new Promise((resolve, reject) => {
     request(option, (error, resp, body) => {
       if (!error && resp.statusCode === 200) {
